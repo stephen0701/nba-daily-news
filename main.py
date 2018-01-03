1117f30838b212046452fad3da22ba9e8b97c7bd
Stephen Fang <stephen.fpc8@gmail.com>
2018-01-03 18:09:19 +0800

    change leveldb declare scope

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from gtts import gTTS
 
 app = Flask(__name__,
             template_folder='templates')
-
-# initialize leveldb
-news_db = db_utils.init("news.db")
             
 @app.route('/')
 def root():
@@ -17,37 +14,9 @@ def root():
 
 @app.route('/main')
 def main():
-    news_titles = db_utils.get_keys(news_db)
-    return render_template('main.html', data=news_titles)
     
-@app.route('/news/<string:date>')
-def news(date):
-    if db_utils.isValid(news_db, date):
-        news_content = db_utils.search(news_db, date)
-        print(news_content)
-        return render_template('content.html', date=date, text=news_content)
-    else:
-        abort(404)
-
-# Convert text to audio and Save
-def generate_mp3(key):
-    filename = "nba_news_{}.mp3".format(key)
-    text = db_utils.search(news_db, key)
-    
-    tts = gTTS(text=text, lang="zh")
-    tts.save("audio\\{}".format(filename))
-    return filename
-        
-# Download audio file from server
-@app.route('/mp3/<string:key>')
-def download(key):
-    
-    filename = generate_mp3(key)
-    
-    path = os.path.join(current_app.root_path, "audio")
-    return send_from_directory(directory=path, filename=filename, as_attachment=True)
-
-if __name__ == "__main__":
+    # initialize leveldb
+    news_db = db_utils.init("news.db")
     
     # Get the latest news
     key = time.strftime("%Y%m%d")
@@ -59,5 +28,45 @@ if __name__ == "__main__":
         value = get_latest_news(key).encode("utf-8")
         db_utils.insert(news_db, key, value)
         print("Add Today NBA News to DB.")
+    
+    news_titles = db_utils.get_keys(news_db)
+    return render_template('main.html', data=news_titles)
+    
+@app.route('/news/<string:date>')
+def news(date):
 
+    # initialize leveldb
+    news_db = db_utils.init("news.db")
+
+    # Render the news website page
+    if db_utils.isValid(news_db, date):
+        news_content = db_utils.search(news_db, date)
+        print(news_content)
+        return render_template('content.html', date=date, text=news_content)
+    else:
+        abort(404)
+        
+# Convert text to audio and Save
+def generate_mp3(db, key):
+    filename = "nba_news_{}.mp3".format(key)
+    text = db_utils.search(db, key)
+    
+    tts = gTTS(text=text, lang="zh")
+    tts.save("audio\\{}".format(filename))
+    return filename
+        
+# Download audio file from server
+@app.route('/mp3/<string:key>')
+def download(key):
+    
+    # initialize leveldb
+    news_db = db_utils.init("news.db")
+    
+    filename = generate_mp3(news_db, key)
+    
+    path = os.path.join(current_app.root_path, "audio")
+    return send_from_directory(directory=path, filename=filename, as_attachment=True)
+
+if __name__ == "__main__":
+    
     app.run(debug=False, port=8080)
